08d5b5f589d055bdac2596d053aa7325175f77d9
Leonor-Rosales <158802869+Leonor-Rosales@users.noreply.github.com>
2024-07-31 09:49:36 -0600

    Libro de recetas culinarias con funciones buscar, mostrar y agregar

---
+++ Actividad 8/Program.cs
@@ -0,0 +1,50 @@
+﻿using Actividad_8;
+
+List<Recetas> RecetasList = new List<Recetas> ();
+Recetas recetasObjetc = new Recetas(RecetasList);
+
+bool run = true;
+do
+{
+    try
+    {
+
+        recetasObjetc.Menu();
+        int option = int.Parse(Console.ReadLine());
+        switch (option)
+        {
+            case 1:
+                Console.Clear();
+                recetasObjetc.IngresarRecetas(RecetasList);
+
+                break;
+            case 2:
+                Console.Clear();
+                recetasObjetc.BuscarReceta(RecetasList);
+              
+                break;
+            case 3:
+                Console.Clear();
+                recetasObjetc.MostrarRecetas(RecetasList);
+               
+                break;
+            case 4:
+                Console.Clear();
+                Console.WriteLine("Saliendo...");
+                Console.ReadKey();
+                run = false;
+                break;
+
+            default:
+                Console.WriteLine("Ingrese una opción válida");
+                Console.ReadKey();
+                break;
+        }
+    }
+    catch (Exception ex)
+    {
+
+        Console.WriteLine("Error: " + ex.Message);
+        Console.ReadKey();
+    }
+} while (run == true);
---
+++ Actividad 8/Recetas.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Actividad_8
+{
+    internal class Recetas
+    {
+        public string Nombre { get; set; }
+        public string Ingredientes { get; set; }
+        public string Preparacion { get; set; }
+
+        public Recetas(string nombre, string ingredientes, string preparacion)
+        {
+            Nombre = nombre;
+            Ingredientes = ingredientes;
+            Preparacion = preparacion;
+        }
+
+        public Recetas(List<Recetas> recetasList)
+        {
+        }
+
+        public void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("--- Recetas ---");
+            Console.WriteLine("1. Agregar Recetas");
+            Console.WriteLine("2. Buscar Recetas");
+            Console.WriteLine("3. Mostrar Recetas");
+            Console.WriteLine("4. Salir");
+            Console.Write("Ingrese una opción: ");
+        }
+
+        public void IngresarRecetas(List<Recetas> recetaList)
+        {
+
+            try
+            {
+              
+                Console.WriteLine("Ingrese el nombre de la receta: ");
+                string nombreReceta = Console.ReadLine() ?? "";
+                nombreReceta.ToLower();
+                Console.WriteLine("Ingrese los ingredientes: ");
+                string ingredientesReceta = Console.ReadLine() ?? "";
+                Console.WriteLine("Ingrese una descripción de la preparación: ");
+                string preparacionReceta = Console.ReadLine() ?? "";
+                Recetas? recetaNombreFind = recetaList.Find(p => p.Nombre == nombreReceta);
+                if (recetaNombreFind == null)
+                {
+                    recetaList.Add(new Recetas(nombreReceta, ingredientesReceta, preparacionReceta));
+                    Console.WriteLine("Receta Añadida Correctamente");
+                    Console.ReadKey();
+                }
+                else
+                {
+                    Console.WriteLine("Receta Existente");
+                    Console.ReadKey();
+                }
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine("[!]Error: "+ ex.Message);
+            }
+        }
+        public void BuscarReceta(List<Recetas> recetaList)
+        {
+            Console.WriteLine("--- Buscar Recetas ---");
+            Console.Write("Ingresa el nombre de la Receta: ");
+            string nombreReceta = Console.ReadLine() ?? "";
+            nombreReceta.ToLower();
+            Recetas? recetasFind = recetaList.Find(p => p.Nombre == nombreReceta);
+            if (recetasFind != null)
+            {
+                Console.WriteLine($"Nombre: {recetasFind.Nombre}\nIngredientes {recetasFind.Ingredientes}\nPreparación: {recetasFind.Preparacion}");
+                Console.WriteLine("");
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("Receta Inexistente");
+                Console.ReadKey();
+            }
+
+        }
+        public void MostrarRecetas(List<Recetas> recetasList)
+        {
+            foreach (var recetas in recetasList)
+            {
+                Console.WriteLine("--- Receta {ID} ---");
+                Console.WriteLine($"Nombre: {recetas.Nombre}\nIngredientes: {recetas.Ingredientes}\nPreparación: {recetas.Preparacion} ");
+            }
+        }
+
+    }
+}
